8e96a5fc51c439e609877567ed1d5643611dab78
zhijxu <zhijxu@microsoft.com>
2022-07-14 00:04:09 +0000

    some improve.
    use size_t instead of unsigned int.
    use memset to make sure mem is actually alloc in OS.

--- series/cuda-cpp/optimize-data-transfers/try.cu
+++ series/cuda-cpp/optimize-data-transfers/try.cu
@@ -48,15 +48,15 @@ typedef struct callBackData {
   void *src;
   void *dst;
   void *expected;
-  unsigned int bytes;
+  size_t bytes;
 } callBackData_t;
-void check_accuracy(int *data, int* expected, unsigned int num)
+void check_accuracy(int *data, int* expected, size_t num)
 {
-    for(int i=0; i<num; i++)
+    for(size_t i=0; i<num; i++)
     {
         if(data[i] != expected[i])
         {
-            printf("something is wrong, total num is %d, index is %d, %d vs %d \n", num, i, data[i], expected[i]);
+            printf("something is wrong, total num is %ld, index is %ld, %d vs %d \n", num, i, data[i], expected[i]);
             return;
         }
     }
@@ -69,37 +69,39 @@ void CUDART_CB memcpyHostToHost(void *ptr) {
     // checkCuda(cudaMemcpy(info->dst, info->src, info->bytes, cudaMemcpyHostToHost));
     memcpy(info->dst, info->src, info->bytes);
     cout << "H2H time: " << chrono::duration_cast<chrono::microseconds>(chrono::steady_clock::now() - start).count()/1000.0 << "ms" << endl;
-    // check_accuracy((int*)info->dst, (int*)info->expected, info->bytes/4);
     // memcmp(info->dst, info->src, info->bytes);
+    // check_accuracy((int*)info->dst, (int*)info->expected, info->bytes/4);
 }
 
 int main()
 {
-  unsigned int nElements = 512*1024*1024;
-  const unsigned int bytes = nElements * sizeof(float);
+  size_t nElements = 1024*1024*1024;
+  size_t bytes = nElements * sizeof(float);
 
   // 2 stream
   int num_streams = 2;
   cudaStream_t streams[num_streams];
-  for (int i = 0; i < num_streams; i++) {
+  for (size_t i = 0; i < num_streams; i++) {
         cudaStreamCreate(&streams[i]);
   }
   // device array
   float *d_a;
-  checkCuda( cudaMalloc((void**)&d_a, bytes) );
-  float *h_expected = (float*)malloc(bytes);
-  for(int i=0; i<nElements; i++){
+  checkCuda( cudaMalloc((void**)&d_a, 2*bytes) );
+  float *h_expected = (float*)malloc(2*bytes);
+  memset(h_expected, 0, 2*bytes);
+  for(size_t i=0; i<2*nElements; i++){
     h_expected[i] = i;
   }
-  checkCuda(cudaMemcpy(d_a, h_expected, bytes, cudaMemcpyHostToDevice));
+  checkCuda(cudaMemcpy(d_a, h_expected, 2*bytes, cudaMemcpyHostToDevice));
   // host arrays
   float *h_aPageable, *h_bPageable;
   float *h_aPinned, *h_bPinned;
-  h_aPageable = (float*)malloc(bytes);                    // host pageable
-  h_bPageable = (float*)malloc(bytes);                    // host pageable
-  checkCuda( cudaMallocHost((void**)&h_aPinned, bytes) ); // host pinned
-  checkCuda( cudaMallocHost((void**)&h_bPinned, bytes) ); // host pinned
-  //
+  h_aPageable = (float*)malloc(bytes);
+  h_bPageable = (float*)malloc(2*bytes);
+  memset(h_aPageable, 0, bytes); // make sure malloc memory is alloc in OS
+  memset(h_bPageable, 0, 2*bytes);
+  checkCuda( cudaMallocHost((void**)&h_aPinned, bytes) );
+  checkCuda( cudaMallocHost((void**)&h_bPinned, 2*bytes) );
   for (int j=0; j<6; j++){
     //stream 0
     cudaMemcpyAsync(h_aPinned, d_a, bytes, cudaMemcpyDeviceToHost, streams[0]);
@@ -110,7 +112,10 @@ int main()
     host_args1->bytes = bytes;
     cudaLaunchHostFunc(streams[0], memcpyHostToHost, host_args1);
     //stream 1
-    cudaMemcpyAsync(h_bPinned, d_a, bytes, cudaMemcpyDeviceToHost, streams[1]);
+    cudaMemcpyAsync(h_bPageable, d_a, 2*bytes, cudaMemcpyDeviceToHost, streams[1]);
+    checkCuda(cudaDeviceSynchronize());
+    // cudaMemcpy(h_bPageable, h_bPinned, 2*bytes, cudaMemcpyHostToHost);
+    continue;
     callBackData_t *host_args2 = new callBackData_t;
     host_args2->src = (void*)h_bPinned;
     host_args2->dst = (void*)h_bPageable;
@@ -120,7 +125,6 @@ int main()
   }
 
     // cleanup
-    checkCuda(cudaDeviceSynchronize());
     cudaFree(d_a);
     cudaFreeHost(h_aPinned);
     cudaFreeHost(h_bPinned);
